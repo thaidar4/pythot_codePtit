735233ff3feabb0bed3e7a5a93bbac4acde81ecb
thaidar4 <114276477+thaidar4@users.noreply.github.com>
2023-10-19 16:16:09 +0700

    Create py01060.py

---
+++ py01060.py
@@ -0,0 +1,21 @@
+def count(num):
+    num = str(num)  
+    sum = 0 
+    product = 1
+    check0 = False
+    for i in range(len(num)):
+        if i % 2 != 0:  
+            sum += int(num[i])
+        elif  i % 2 == 0 and int(num[i]) != 0: 
+            product *= int(num[i])
+            check0 = True
+        
+    if check0 == False :
+        product = 0
+    print (product ,sum )
+    
+
+
+for loop in range(int(input())):
+    num = input()
+    count(num)
